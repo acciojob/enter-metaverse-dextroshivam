6ad24df0e248bd9128de843ef5d29429f4121bef
shivam <76024545+dextroshivam@users.noreply.github.com>
2024-02-04 16:47:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let ptag=document.getElementById("status");
+let btn=document.getElementById("enterBtn");
+
+btn.addEventListener("click",()=>{
+	ptag.innerHTML=`<h1>Entered Metaverse</h1>`;
+});
